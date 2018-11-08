ca0348c3a39baa63019202d9a94de3ebc4b096bc
davidshiller <davidshiller@conor.co.za>
2018-11-08 21:17:33 +0200

    Added Basic SQL Lite support to handle creating tables on controller initilization

--- controller/main.go
+++ controller/main.go
@@ -6,6 +6,8 @@ import (
 	"encoding/json"
 	"fmt"
 	"time"
+	"database/sql"
+	_ "github.com/mattn/go-sqlite3"
 )
 
 type RegisterationRequest struct {
@@ -53,30 +55,54 @@ func _setRGB(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	go _health()
 	http.HandleFunc("/register", _register)
 	http.HandleFunc("/devices", _devices)
 	http.HandleFunc("/set-rgb", _setRGB)
 
-	go func() {
-		for {
-			time.Sleep(10 * time.Second)
-			for _, device := range devices {
-				url := fmt.Sprintf("http://%v:%v/health", device.ip, device.port )
-				resp, err := http.Get(url)
-				if err != nil {
-					panic(err)
-				}
-				respBody, err := ioutil.ReadAll(resp.Body)
-				fmt.Println((string)(respBody))
-			}
-		}
-	}()
+
 
 	if err := http.ListenAndServe(":8090", nil); err != nil {
 		panic(err)
 	}
-
-
-
 }
 
+
+func _health() {
+	for {
+		time.Sleep(10 * time.Second)
+		for _, device := range devices {
+			url := fmt.Sprintf("http://%v:%v/health", device.ip, device.port )
+			resp, err := http.Get(url)
+			if err != nil {
+				panic(err)
+			}
+			respBody, err := ioutil.ReadAll(resp.Body)
+			fmt.Println((string)(respBody))
+		}
+	}
+}
+
+func init() {
+	//TODO: Check if the desk, group, deskgroup tables are created if not create them
+	db, err := sql.Open("sqlite3", "./ggwp.db")
+	if err != nil {
+		fmt.Println(err)
+		panic(err)
+	}
+	db.Exec(`PRAGMA foreign_keys=ON`)
+	tblDesk, err := db.Prepare("CREATE TABLE IF NOT EXISTS desk (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, ip TEXT, port TEXT, red INTEGER, green INTEGER, blue INTEGER, health TEXT, retries INTEGER)")
+	if err != nil {
+		fmt.Println(err)
+		panic(err)
+	}
+	tblDesk.Exec()
+	tblGroup, err := db.Prepare("CREATE TABLE IF NOT EXISTS collection (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT)")
+	if err != nil {
+		fmt.Println(err)
+		panic(err)
+	}
+	tblGroup.Exec()
+	db.Exec("CREATE TABLE IF NOT EXISTS deskcollection(id INTEGER PRIMARY KEY AUTOINCREMENT, collection_id INTEGER, desk_id INTEGER FOREIGN KEY collection_id REFERENCES collection(id))")
+	fmt.Println("func[init] initilization complete")
+	}
